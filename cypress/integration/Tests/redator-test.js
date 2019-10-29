a1432f885e3e7488bcdcc83a1ea4d5149e3ba6e8
Daniel <danielmarlon181@gmail.com>
2019-10-29 20:19:30 -0300

    updating redator-test.js

--- cypress/integration/Tests/redator-test.js
+++ cypress/integration/Tests/redator-test.js
@@ -1,15 +1,16 @@
-describe('redator login test', function() {
+describe('redator role test', function() {
     it('Visit Ast site', function() {
         cy.visit('ast.docker.local')
-
-        cy.contains('Entrar').click()
-        cy.get('input[name="name"]').type('Redator')
-        cy.get('input[name="pass"]').type('asteste21')
-        cy.get('input[name="op"]').click()
-        cy.contains('Adicionar conteúdo').click()
-        cy.contains('Notícias').click()
-        cy.get('input[name="title[0][value]"]').type('test')
-        cy.get('div[id="cke_1_contents"]').type('isso aqui')
-
-      })
     })
+
+    it('logging in as a Redator and creating Notícias content', function() {
+      cy.contains('Entrar').click()
+      cy.get('input[name="name"]').type('Redator')
+      cy.get('input[name="pass"]').type('asteste21')
+      cy.get('input[name="op"]').click()
+      cy.contains('Adicionar conteúdo').click()
+      cy.contains('Notícias').click()
+      cy.get('input[name="title[0][value]"]').type('test')
+      cy.get('div[id="cke_1_contents"]').type('isso aqui')
+    })
+})
