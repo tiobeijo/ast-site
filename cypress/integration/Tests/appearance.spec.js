10c819d820fe8bc9949152fbf72d15c5a1075618
AST <srmoura@outlook.com>
2019-10-30 14:31:10 -0300

    appearance test fixed

--- cypress/integration/Tests/appearance.spec.js
+++ cypress/integration/Tests/appearance.spec.js
@@ -8,27 +8,21 @@ describe("Color test", () => {
           .should('have.css', 'background-color')
           .and('eq', 'rgb(255, 0, 0)')
     })
-  })
 
-describe("Color test", () => {
     it('Verify the button backgroud color, this should work', () => {
       cy.visit('ast.docker.local')
        cy.get('.form-submit')
          .should('have.css', 'background-color')
          .and('eq', 'rgb(255, 0, 0)')
    })
-}) 
 
-describe("Color test", () => {
   it('Verify the title color, this should work', () => {
     cy.visit('ast.docker.local')
      cy.get('h1')
        .should('have.css', 'background-color')
        .and('eq', 'rgba(0, 0, 0, 0)')
  })
-}) 
-
-describe("Color test", () => {
+ 
   it('Verify the letters color, this should work', () => {
     cy.visit('ast.docker.local')
      cy.get('a')
