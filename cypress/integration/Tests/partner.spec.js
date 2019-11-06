7b267aec7c62f59568d6e154257e24afd9a55de8
Samantha <srmoura@outlook.com.br>
2019-11-06 15:08:51 -0300

    automated testing of the Parceiros content type created

---
+++ cypress/integration/Tests/partner.spec.js
@@ -0,0 +1,41 @@
+describe('redator role test', function() {
+    it('Visit Ast site', function() {
+        cy.visit('ast.docker.local')
+    })
+
+    it('logging in as a Redator and creating Parceiro content', function() {
+      cy.contains('Log in').click()
+      cy.get('input[name="name"]').type('redator01')
+      cy.get('input[name="pass"]').type('redatorpass')
+      cy.get('input[name="op"]').click()
+      cy.contains('Conteúdo').click()
+      cy.visit('ast.docker.local/node/add')
+      cy.contains('Parceiro').click()
+      cy.get('input[name="title[0][value]"]').type('Ralph Lauren')
+      cy.window()
+  .then(win => {
+    win.CKEDITOR.instances["edit-body-0-value"]
+    .setData("<p>A Ralph Lauren Corporation é uma empresa de moda americana que produz produtos que variam desde os segmentos intermediário até o de luxo. Eles são conhecidos pelo vestuário, marketing e distribuição de produtos em quatro categorias: vestuário, casa, acessórios e fragrâncias.</p>");
+  });
+  cy.wait(2000);
+
+      const imagePath = 'images/ralph_lauren.jpg';
+
+      cy.fixture(imagePath).then(content => {
+        cy.get('input[name="files[field_partner_logo_0]"]').upload([{fileContent: content, fileName: 'ralph_lauren.jpg', mimeType: 'image/JPEG'}
+          ]);
+      });
+
+      cy.get('input[id="edit-field-reference-site-0-uri"]').type('https://www.ralphlauren.com/')
+
+      cy.wait(2000);
+
+      cy.contains('Save').click()
+    })
+
+    it('checking Parceiros content', function() {
+
+      cy.contains('Ralph Lauren')
+      cy.contains("A Ralph Lauren Corporation é uma empresa de moda americana que produz produtos que variam desde os segmentos intermediário até o de luxo. Eles são conhecidos pelo vestuário, marketing e distribuição de produtos em quatro categorias: vestuário, casa, acessórios e fragrâncias.")
+    })
+})
